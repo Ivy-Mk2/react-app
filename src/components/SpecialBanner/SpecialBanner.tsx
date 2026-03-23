d67a90d5eefd64c7203ff24d33d7e7ab67081611
Lomins <deadlomins@gmail.com>
2026-03-23 17:00:57 -0500

    Refactor SpecialBanner to use local image source instead of external URL

--- src/components/SpecialBanner/SpecialBanner.tsx
+++ src/components/SpecialBanner/SpecialBanner.tsx
@@ -15,7 +15,7 @@ const SpecialBanner =() =>{
                         ))}
                     </div>
                     <div className="special-banner__image-container">
-                        <img src="https://www.off---white.com/BWStaticContent/53000/ade9d580-d18c-4bad-a4b4-11f7e7f8f25d_skate-caps-web.jpg" alt="Colección de caballeros" className="special-banner__image"/>
+                        <img src={Banner} alt="Colección de caballeros" className="special-banner__image"/>
                     </div>
                 </div>
                 <div className="special-banner__block special-banner__block--2">
